b518b796975283bb6e210ca65d786896891e121e
Hussein Najah <husseinnajah123@gmail.com>
2023-07-13 03:26:19 +0300

    done for now!

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,11 @@
-const rootPage = () => {
-  return <div className="p-4">this is a protected page</div>;
+import { UserButton } from "@clerk/nextjs";
+
+const SetUpPage = () => {
+  return (
+    <div className="p-4">
+      this is a protected page
+      <UserButton />
+    </div>
+  );
 };
-export default rootPage;
+export default SetUpPage;

991d0ff0681a8d81ea89822d5332f9a10616fa31
Hussein Najah <husseinnajah123@gmail.com>
2023-07-13 23:23:51 +0300

    heading

---
+++ components/ui/heading.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+interface HeadingProps {
+  title: string;
+  description: string;
+}
+
+export const Heading: React.FC<HeadingProps> = ({ title, description }) => {
+  return (
+    <div className="flex items-center justify-between">
+      <div>
+        <h2 className="text-2xl font-semibold text-black dark:text-white">
+          {title}
+        </h2>
+        <p className="text-sm text-muted-foreground">{description}</p>
+      </div>
+    </div>
+  );
+};

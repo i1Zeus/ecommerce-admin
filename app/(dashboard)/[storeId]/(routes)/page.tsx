c9c0e2d6a0a9ddef4cc9cf0acc138e660484c2e4
Hussein Najah <husseinnajah123@gmail.com>
2023-07-13 20:31:38 +0300

    dash

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,5 +1,22 @@
-const DashboardPage = () => {
-  return <div>Dashboard Page</div>;
+import prismadb from "@/lib/prismadb";
+
+interface DashboardPageProps {
+  params: {
+    storeId: string;
+  };
+}
+
+const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
+  const store = await prismadb.store.findUnique({
+    where: { id: params.storeId },
+  });
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>Store ID: {store?.name}</p>
+    </div>
+  );
 };
 
 export default DashboardPage;

bc82d85b4b1a20098e79e7e927cb85aa42db5221
Hussein Najah <husseinnajah123@gmail.com>
2023-07-13 14:48:02 +0300

    reOrder

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
-import "./globals.css";
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+
+import type { Metadata } from "next";
 import { ModalProvider } from "@/providers/modal-provider";
+
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 

765bc2cf566176da83a1c3fa99d760206b3ab71f
Hussein Najah <husseinnajah123@gmail.com>
2023-07-14 21:49:24 +0300

    its not editing or delering.

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
@@ -41,11 +41,12 @@ type BillboardFormValues = z.infer<typeof fromSchema>;
 export const BillboardForm: React.FC<BillboardFormProps> = ({
   initialData,
 }) => {
-  const [open, setOpen] = useState(false);
-  const [loading, setLoading] = useState(false);
   const params = useParams();
   const router = useRouter();
   const origin = useOrigin();
+
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const title = initialData ? "Edit billboard" : "Add billboard";
   const description = initialData ? "Edit a billboard" : "Add a New billboard";

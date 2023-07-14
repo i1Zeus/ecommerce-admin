89c0e86e3e1459abd5b62f367c565eec10eeb68a
Hussein Najah <husseinnajah123@gmail.com>
2023-07-14 20:26:19 +0300

    patch billboard

---
+++ app/api/[storeId]/billboards/billboardId/route.ts
@@ -0,0 +1,49 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { storeId: string; billboardId: string } }
+) {
+  try {
+    const { userId } = auth();
+    if (!userId) return new NextResponse("Unauthorized", { status: 401 });
+
+    const body = await req.json();
+
+    const { label, imageUrl } = body;
+    if (!label) return new NextResponse("Label is required", { status: 400 });
+    if (!imageUrl)
+      return new NextResponse("Image URL is required", { status: 400 });
+
+    if (!params.billboardId)
+      return new NextResponse("Billboard Id is required", { status: 400 });
+
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    if (!storeByUserId)
+      return new NextResponse("Unauthorized", { status: 401 });
+
+    const billboard = await prismadb.billboard.updateMany({
+      where: {
+        id: params.billboardId,
+      },
+      data: {
+        label,
+        imageUrl,
+      },
+    });
+
+    return NextResponse.json(billboard);
+  } catch (err) {
+    console.log("[Billboard_PATCH]", err);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
+

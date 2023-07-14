fb5658fa6ad044bbb1ef7ae8557f2b1a477e4598
Hussein Najah <husseinnajah123@gmail.com>
2023-07-14 20:26:32 +0300

    delete billboard

--- app/api/[storeId]/billboards/billboardId/route.ts
+++ app/api/[storeId]/billboards/billboardId/route.ts
@@ -47,3 +47,27 @@ export async function PATCH(
   }
 }
 
+export async function DELETE(
+  _req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    const { userId } = auth();
+    if (!userId) return new NextResponse("Unauthorized", { status: 401 });
+
+    if (!params.storeId)
+      return new NextResponse("Store is required", { status: 400 });
+
+    const store = await prismadb.store.deleteMany({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    return NextResponse.json(store);
+  } catch (err) {
+    console.log("[Stores_DELETE]", err);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}

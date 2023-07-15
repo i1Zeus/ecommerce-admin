0424aa839f9e043355e1fe02037a4a879bf00c9c
Hussein Najah <husseinnajah123@gmail.com>
2023-07-15 23:01:03 +0300

    color cell color

--- app/(dashboard)/[storeId]/(routes)/colors/components/columns.tsx
+++ app/(dashboard)/[storeId]/(routes)/colors/components/columns.tsx
@@ -18,6 +18,14 @@ export const columns: ColumnDef<ColorColumn>[] = [
   {
     accessorKey: "value",
     header: "Value",
+    cell: ({ row }) => (
+      <div className="flex items-center gap-x-2">
+        <div
+          className={`h-6 w-6 rounded-full border bg-[${row.original.value}]`}
+        />
+        {row.original.value}
+      </div>
+    ),
   },
   {
     accessorKey: "createdAt",

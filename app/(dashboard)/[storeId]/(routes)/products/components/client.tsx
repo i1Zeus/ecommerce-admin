da46af138e9c3c73e2c690a42fab1eb558ea8f4a
Hussein Najah <husseinnajah123@gmail.com>
2023-07-16 00:21:17 +0300

    Products

--- app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
@@ -5,36 +5,34 @@ import { useParams, useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
-import { BillboardColumn, columns } from "./columns";
+import { ProductColumn, columns } from "./columns";
 import { DataTable } from "@/components/ui/data-table";
 import { ApiList } from "@/components/ui/api-list";
 
-interface BillboardClientProps {
-  data: BillboardColumn[];
+interface ProductClientProps {
+  data: ProductColumn[];
 }
 
-export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
+export const ProductClient: React.FC<ProductClientProps> = ({ data }) => {
   const router = useRouter();
   const params = useParams();
   return (
     <>
       <div className="flex items-center justify-between ">
         <Heading
-          title={`Billboard (${data.length})`}
-          description="Manage you'r store billboard."
+          title={`Products (${data.length})`}
+          description="Manage you'r store products."
         />
-        <Button
-          onClick={() => router.push(`/${params.storeId}/billboards/new`)}
-        >
+        <Button onClick={() => router.push(`/${params.storeId}/products/new`)}>
           <Plus className="w-4 h-4 mr-2" />
           Add New
         </Button>
       </div>
       <Separator />
-      <DataTable columns={columns} data={data} filterKey="label" />
-      <Heading title="API" description="API calls for Billboards." />
+      <DataTable columns={columns} data={data} filterKey="name" />
+      <Heading title="API" description="API calls for Products." />
       <Separator />
-      <ApiList entityName="billboards" entityIdName="billboardId" />
+      <ApiList entityName="products" entityIdName="productId" />
     </>
   );
 };
--- app/(dashboard)/[storeId]/(routes)/products/components/columns.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/components/columns.tsx
@@ -3,16 +3,54 @@
 import { ColumnDef } from "@tanstack/react-table";
 import { CellAction } from "./cell-action";
 
-export type BillboardColumn = {
+export type ProductColumn = {
   id: string;
-  label: string;
+  name: string;
+  price: string;
+  category: string;
+  size: string;
+  color: string;
+  isFeatured: boolean;
+  isArchived: boolean;
   createdAt: string;
 };
 
-export const columns: ColumnDef<BillboardColumn>[] = [
+export const columns: ColumnDef<ProductColumn>[] = [
   {
-    accessorKey: "label",
-    header: "Label",
+    accessorKey: "name",
+    header: "Name",
+  },
+  {
+    accessorKey: "isArchived",
+    header: "Archived",
+  },
+  {
+    accessorKey: "isFeatured",
+    header: "Featured",
+  },
+  {
+    accessorKey: "price",
+    header: "Price",
+  },
+  {
+    accessorKey: "category",
+    header: "Category",
+  },
+  {
+    accessorKey: "size",
+    header: "Size",
+  },
+  {
+    accessorKey: "color",
+    header: "Color",
+    cell: ({ row }) => (
+      <div className="flex items-center gap-x-2">
+        <div
+          className={`h-6 w-6 rounded-full border bg-[${row.original.color}]`}
+        />
+        {row.original.color}
+      </div>
+    ),
   },
   {
     accessorKey: "createdAt",

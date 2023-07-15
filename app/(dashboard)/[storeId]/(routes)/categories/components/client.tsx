4622a9f0574d7ff9723203099cff671ba7bed371
Hussein Najah <husseinnajah123@gmail.com>
2023-07-15 17:58:50 +0300

    category filterKey

--- app/(dashboard)/[storeId]/(routes)/categories/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/components/client.tsx
@@ -31,7 +31,7 @@ export const CategoryClient: React.FC<CategoryClientProps> = ({ data }) => {
         </Button>
       </div>
       <Separator />
-      <DataTable columns={columns} data={data} filterKey="label" />
+      <DataTable columns={columns} data={data} filterKey="name" />
       <Heading title="API" description="API calls for Categories" />
       <Separator />
       <ApiList entityName="categories" entityIdName="categoriesId" />

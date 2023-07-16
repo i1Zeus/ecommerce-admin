df677baa660a98f9d43eb9d017283942a9aa6aa0
Hussein Najah <husseinnajah123@gmail.com>
2023-07-16 16:54:00 +0300

    api alert

--- components/ui/api-alert.tsx
+++ components/ui/api-alert.tsx
@@ -28,7 +28,7 @@ export const ApiAlert: React.FC<ApiAlertProps> = ({
 }) => {
   const onCopy = (description: string) => {
     navigator.clipboard.writeText(description);
-    toast.success("API Route copied to clipboard.");
+    toast.success("Copied to clipboard.");
   };
 
   return (

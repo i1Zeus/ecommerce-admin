f4e8c0efbc796554e8e7a413a4fb56e77e087dec
Hussein Najah <husseinnajah123@gmail.com>
2023-07-19 01:52:29 +0300

    loader

--- components/ui/loader.tsx
+++ components/ui/loader.tsx
@@ -1,7 +1,15 @@
 "use client";
 
+import { useTheme } from "next-themes";
 import { ClipLoader } from "react-spinners";
 
 export const Loader = () => {
-  return <ClipLoader color="#000000" size={65} />;
+  const theme = useTheme();
+
+  return (
+    <ClipLoader
+      color={theme.resolvedTheme === "dark" ? "#ffffff" : "#000000"}
+      size={65}
+    />
+  );
 };

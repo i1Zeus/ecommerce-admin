3e43c57aca01ad4b8ebe3617de84b5af10fc413e
Hussein Najah <husseinnajah123@gmail.com>
2023-07-18 18:56:28 +0300

    overview

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,13 +1,13 @@
 import { CreditCard, DollarSign, Package } from "lucide-react";
 
 import { Separator } from "@/components/ui/separator";
-// import { Overview } from "@/components/overview";
+import { Overview } from "@/components/overview";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Heading } from "@/components/ui/heading";
-// import { getTotalRevenue } from "@/actions/get-total-revenue";
-// import { getSalesCount } from "@/actions/get-sales-count";
-// import { getGraphRevenue } from "@/actions/get-graph-revenue";
-// import { getStockCount } from "@/actions/get-stock-count";
+import { getTotalRevenue } from "@/actions/get-total-revenue";
+import { getSalesCount } from "@/actions/get-sales-count";
+import { getGraphRevenue } from "@/actions/get-graph-revenue";
+import { getStockCount } from "@/actions/get-stock-count";
 import { formatter } from "@/lib/utils";
 
 interface DashboardPageProps {
@@ -17,10 +17,10 @@ interface DashboardPageProps {
 }
 
 const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
-  // const totalRevenue = await getTotalRevenue(params.storeId);
-  // const graphRevenue = await getGraphRevenue(params.storeId);
-  // const salesCount = await getSalesCount(params.storeId);
-  // const stockCount = await getStockCount(params.storeId);
+  const totalRevenue = await getTotalRevenue(params.storeId);
+  const graphRevenue = await getGraphRevenue(params.storeId);
+  const salesCount = await getSalesCount(params.storeId);
+  const stockCount = await getStockCount(params.storeId);
 
   return (
     <div className="flex-col">
@@ -37,7 +37,7 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
             </CardHeader>
             <CardContent>
               <div className="text-2xl font-bold">
-                {/* {formatter.format(totalRevenue)} */}
+                {formatter.format(totalRevenue)}
               </div>
             </CardContent>
           </Card>
@@ -47,7 +47,7 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
               <CreditCard className="h-4 w-4 text-muted-foreground" />
             </CardHeader>
             <CardContent>
-              {/* <div className="text-2xl font-bold">+{salesCount}</div> */}
+              <div className="text-2xl font-bold">+{salesCount}</div>
             </CardContent>
           </Card>
           <Card>
@@ -58,7 +58,7 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
               <Package className="h-4 w-4 text-muted-foreground" />
             </CardHeader>
             <CardContent>
-              {/* <div className="text-2xl font-bold">{stockCount}</div> */}
+              <div className="text-2xl font-bold">{stockCount}</div>
             </CardContent>
           </Card>
         </div>
@@ -67,7 +67,7 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
             <CardTitle>Overview</CardTitle>
           </CardHeader>
           <CardContent className="pl-2">
-            {/* <Overview data={graphRevenue} /> */}
+            <Overview data={graphRevenue} />
           </CardContent>
         </Card>
       </div>

73a7da11f0b26213e298a6fb48fbcacb3d537fa6
Hussein Najah <husseinnajah123@gmail.com>
2024-01-26 18:58:11 +0300

    middle

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/api/webhook"],
+  ignoredRoutes: ["/api", "/api/:path*"],
 });
 
 export const config = {
